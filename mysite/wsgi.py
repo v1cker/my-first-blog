a8c99929b58ae0bf1e2afe083d18eb5320c23184
Yan Jiang <j582033@163.com>
2016-05-19 23:40:08 +0800

    V 1.0

---
+++ mysite/wsgi.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+path = '/home/vicker/my-first-blog'  # use your own username here
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(get_wsgi_application())
